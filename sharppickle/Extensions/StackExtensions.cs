957044ad0d65485734b9e1e3b4c4bc7607d019aa
Fabian Creutz <kaskadee0@gmail.com>
2020-05-19 22:10:02 +0200

    Added documentation for Stack.Peek{T} and moved to Extension namespace

---
+++ sharppickle/Extensions/StackExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using sharppickle.Exceptions;
+
+namespace sharppickle.Extensions {
+    /// <summary>
+    ///     Provides extension methods to provide additional features for the <see cref="Stack{T}"/> class.
+    /// </summary>
+    internal static class StackExtensions {
+        /// <summary>
+        ///     Returns the first element of the <see cref="Stack"/> without removing it from the top.
+        /// </summary>
+        /// <typeparam name="T">The expected type of the object.</typeparam>
+        /// <param name="stack">The stack to peek the first element from.</param>
+        /// <returns>The top object of the <paramref name="stack"/> as the specified type.</returns>
+        /// <exception cref="UnpicklingException">The element is of type {type} but {T} was expected.</exception>
+        public static T Peek<T>(this Stack stack) {
+            var obj = stack.Peek();
+            if(!(obj is T value))
+                throw new UnpicklingException($"The element is of type {obj.GetType().Name} but {typeof(T).Name} was expected.");
+            return value;
+        }
+    }
+}
