8be6d052fb20742d32224092d180d4a222ec2aec
Fabian Creutz <kaskadee0@gmail.com>
2020-05-19 22:12:09 +0200

    Added documentation for PythonObject

--- sharppickle/PythonObject.cs
+++ sharppickle/PythonObject.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace sharppickle {
+﻿namespace sharppickle {
     /// <summary>
-    ///     Provides a base class to implement a python object, which can be unpickled using <see cref="PickleReader"/>.
+    ///     Provides a template to implement a Python object, which can be deserialized using <see cref="PickleReader"/>.
     /// </summary>
     public abstract class PythonObject {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PythonObject"/> class.
+        /// </summary>
         protected PythonObject() { }
 
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PythonObject"/> class.
+        /// </summary>
+        /// <param name="args">The arguments to create the object with.</param>
         protected PythonObject(params object[] args) { }
 
+        /// <summary>
+        ///     Sets the state of the object.
+        /// </summary>
+        /// <param name="obj">The object state to apply.</param>
         public abstract void SetState(object obj);
     }
 }
