00962f6b77fdd5a94c590e51e8864f9714c0aa37
Fabian Creutz <kaskadee0@gmail.com>
2020-05-19 22:10:16 +0200

    Added StreamExtensions

---
+++ sharppickle/Extensions/StreamExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Text;
+
+namespace sharppickle.Extensions {
+    /// <summary>
+    ///     Provides extension methods to simplify retrieving data from a <seealso cref="Stream"/>.
+    /// </summary>
+    internal static class StreamExtensions {
+        /// <summary>
+        ///     Reads a string from the specified stream until a new line character (\n) has been read.
+        /// </summary>
+        /// <param name="stream">The <see cref="Stream"/> to read the string from.</param>
+        /// <returns>The string read from the specified <see cref="Stream"/>.</returns>
+        public static string ReadLine(this Stream stream) {
+            var sb = new StringBuilder();
+            while (stream.Position < stream.Length) {
+                var b = (char)stream.ReadByte();
+                if (b == '\n')
+                    break;
+                sb.Append(b);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
